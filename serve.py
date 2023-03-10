cab9864ca2116935ed6f24a1b86b6141b583533c
leowangx2013 <leowangx2013@outlook.com>
2023-03-10 02:56:28 -0600

    Change serve.py to returning ip

--- serve.py
+++ serve.py
@@ -1,17 +1,20 @@
 from flask import Flask, request
+import socket
 
 app = Flask(__name__)
 
-seed_number = [0]
+# seed_number = [0]
 @app.route("/", methods=["GET", "POST"])
 def serve():
-    if request.method == 'POST':
-        data = request.get_json()
-        print(data)
-        seed_number[0] = data['num']
-        return "success"
+    # if request.method == 'POST':
+    #     data = request.get_json()
+    #     print(data)
+    #     seed_number[0] = data['num']
+    #     return "success"
     if request.method == 'GET':
-        return str(seed_number[0])
-    
+        # return str(seed_number[0])
+        hostname = socket.gethostname()
+        return socket.gethostbyname(hostname)
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port=8000)
