37ad129d9123459a1569eb07d4723f6b977c9ff2
leowangx2013 <leowangx2013@outlook.com>
2023-03-11 12:36:14 -0600

    Stress CPU based on POST parameters

--- serve.py
+++ serve.py
@@ -5,19 +5,22 @@ import subprocess
 app = Flask(__name__)
 
 # seed_number = [0]
-proc = None
+proc = [None]
 @app.route("/", methods=["GET", "POST"])
 def serve():
     if request.method == 'POST':
-        if proc is not None:
-            proc.terminate()
+        if proc[0] is not None:
+            proc[0].terminate()
         data = request.get_json()
         print(data)
         usage = data['cpu_usage']
-        proc = subprocess.Popen(["stress-ng", "--cpu", "1", "--cpu-load", str(usage)])
-        hostname = socket.gethostname()
-        return f"push {socket.gethostbyname(hostname)} to {usage}"
-    
+        if int(usage) == 0:
+            return "stop stressing CPU"
+        else:
+            proc[0] = subprocess.Popen(["stress-ng", "--cpu", "1", "--cpu-load", str(usage)])
+            hostname = socket.gethostname()
+            return f"push {socket.gethostbyname(hostname)} to {usage}"
+
     if request.method == 'GET':
         hostname = socket.gethostname()
         return socket.gethostbyname(hostname)
